6421188b72c13cf82a883f8080944f5020b074e4
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-03-12 15:46:48 +0530

    Database connetion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDataBase from "./database/index.js";
 import dotenv from "dotenv"
 
@@ -5,4 +6,16 @@ dotenv.config({
     path: "./env"
 }); 
 
-connectDataBase();     
+connectDataBase() 
+.then(() =>{
+    app.listen(process.env.PORT ||  3000, () => {
+        console.log(`Server is running at : ${process.env.PORT}`);
+    }); 
+
+    app.on("error", (error) => {
+        console.log(`Error in starting server : ${error}`)
+    })
+})
+.catch((error) => {
+    console.log(`Mongodb connection error : ${error}`)
+  })
