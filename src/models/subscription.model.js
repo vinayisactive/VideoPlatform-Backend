b50c2ccf7b6337f2bcde6c2e32e4605e245b537b
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-03-21 01:19:36 +0530

    subscription model added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema({
+
+    channel: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    subscriber: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+    }
+    
+        
+    
+},{timestamps: true}); 
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema); 
+
+export default Subscription; 
